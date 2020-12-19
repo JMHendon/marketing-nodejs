56b923d0f855b24cf18e069f4a9f60a8e7d449da
Jeremy Hendon <hidohebhi@gmail.com>
2020-12-18 20:42:08 -0600

    Update Contacts.js

--- src/Contacts.js
+++ src/Contacts.js
@@ -365,6 +365,8 @@ class Contacts {
       'options': options
     };
 
+    console.log(contact);
+
     contact = this.api._discardNullAndEmptyValues(contact);
 
     return this.api._post('', [], contact);
